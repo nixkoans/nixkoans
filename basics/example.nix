63dfbc300ab16bee91b246b8d15f2d3528b9f05c
Calvin Cheng <linchuan.cheng@gmail.com>
2015-05-19 16:25:16 +0800

    Clean up

--- basics/example.nix
+++ basics/example.nix
@@ -1,3 +1,10 @@
-{
-  square=(x: x * x);
-}
+# Run this as
+# nix-instantiate --eval example.nix
+let
+ # square : int -> int
+ square = x: x*x;
+
+ # sumOfSquares : int -> int -> int
+ sumOfSquares = x: y: square x + square y;
+in
+ sumOfSquares 3 7

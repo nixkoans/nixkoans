45eeb60de3624aa7964295e6379c97f075ff807b
Calvin Cheng <linchuan.cheng@gmail.com>
2015-05-22 14:16:03 +0200

    Analyzing our build and run-time dependencies

---
+++ genericbuilders_runtimedependencies/README.md
@@ -0,0 +1,7 @@
+# Build runtime dependencies
+
+The `drv` path for `hello.nix` is shown by `nix-instantiate`:
+
+```
+$ nix-instantiate hello.nix
+```
---
+++ genericbuilders_runtimedependencies/builder.sh
@@ -0,0 +1,19 @@
+set -e
+unset PATH
+for p in $baseInputs $buildInputs; do
+  echo $p
+  export PATH=$p/bin${PATH:+:}$PATH
+done
+
+tar -xf $src
+
+for d in *; do
+  if [ -d "$d" ]; then
+    cd "$d"
+    break
+  fi
+done
+
+./configure --prefix=$out
+make
+make install
